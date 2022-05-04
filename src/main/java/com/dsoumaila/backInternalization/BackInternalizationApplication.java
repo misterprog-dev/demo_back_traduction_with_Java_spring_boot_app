b547c20cc180867f9db92da5582c0f6c49903806
Soumaila DIAKITE <soum.diakite@outlook.com>
2022-05-04 20:35:34 +0200

    Initiation of project

---
+++ src/main/java/com/dsoumaila/backInternalization/BackInternalizationApplication.java
@@ -0,0 +1,13 @@
+package com.dsoumaila.backInternalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackInternalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackInternalizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsoumaila/backInternalization/BackInternalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsoumaila.backInternalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackInternalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
